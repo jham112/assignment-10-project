92644b3bb76c5bacd6184dd8e2f99b9e8714cf28
jham112 <jham112@ufl.edu>
2017-11-03 23:53:24 -0400

    Completed assignment

---
+++ js/action.js
@@ -0,0 +1,56 @@
+var img1 = null;
+var img2 = null;
+var img3 = null;
+var img4 = null;
+
+
+
+
+function init(){
+  img1 = document.getElementById('img1');
+    img1.style.position= 'relative';
+    img1.style.left = '0px';
+}
+
+function moveRight(){
+    img1.style.left = parseInt(img1.style.left) + 10 + 'px';
+}
+
+ window.onload=init
+
+
+ function init(){
+   img2 = document.getElementById('img1');
+     img2.style.position= 'relative';
+     img2.style.left = '0px';
+ }
+
+ function moveRight(){
+     img2.style.left = parseInt(img2.style.left) + 10 + 'px';
+ }
+
+  window.onload=init
+
+  function init(){
+    img3 = document.getElementById('img1');
+      img3.style.position= 'relative';
+      img3.style.left = '0px';
+  }
+
+  function moveRight(){
+      img3.style.left = parseInt(img3.style.left) + 10 + 'px';
+  }
+
+   window.onload=init
+
+   function init(){
+     img4 = document.getElementById('img1');
+       img4.style.position= 'relative';
+       img4.style.left = '0px';
+   }
+
+   function moveRight(){
+       img4.style.left = parseInt(img4.style.left) + 10 + 'px';
+   }
+
+    window.onload=init
